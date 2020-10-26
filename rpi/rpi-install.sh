52ff5f9a58e2a297bd735ca9fa5bdb71b7ac2003
Luke Wegryn <luke@pensivesecurity.io>
2020-10-26 09:18:10 -0400

    Check for docker

--- rpi/rpi-install.sh
+++ rpi/rpi-install.sh
@@ -2,6 +2,11 @@
 
 sudo apt update
 sudo apt upgrade -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+
+if ! command -v COMMAND &> /dev/null
+then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
+
 docker run hello-world
