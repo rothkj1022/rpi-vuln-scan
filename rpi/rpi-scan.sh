62e2fcc004114ef28fb15f693739806b73df4c28
Luke Wegryn <luke@pensivesecurity.io>
2020-10-27 16:01:23 -0400

    Working scan configuration

--- rpi/rpi-scan.sh
+++ rpi/rpi-scan.sh
@@ -6,6 +6,9 @@ ip_subnet=$(echo $ip_addr | cut -d'.' -f1,2,3)
 ip_subnet+=".0/24"
 echo "Scanning $ip_subnet..."
 curl "https://rpi.pensivesecurity.io/sendstart?recipient=$email"
-sudo docker run --rm -v $(pwd):/reports/:rw pensivesecurity/rpi-scanner:latest python3 -u scan.py "$ip_subnet" --update --format="PDF" -m 256 --output rpi-openvas-report.pdf --profile="Full and fast"
-file_url=$(sudo docker run --rm -it -v $(pwd):/data timvisee/ffsend upload -h https://pensivesecurity.io/ rpi-openvas-report.pdf)
-curl "https://rpi.pensivesecurity.io/sendresults?recipient=$email&reporturl=$file_url"
+docker run --rm -v $(pwd):/reports/:rw pensivesecurity/rpi-scanner:latest python3 -u scan.py "$ip_subnet" --debug --format="PDF" --output rpi-openvas-report.pdf --profile="Full and fast"
+file_url=$(docker run --rm -it -v $(pwd):/data pensivesecurity/ffsend-rpi upload --expiry-time 5d --downloads 5 -q -h https://send.pensivesecurity.io/ rpi-openvas-report.pdf | sed 's,http,https,g')
+curl -G \
+    --data-urlencode "recipient=$email" \
+    --data-urlencode "reporturl=$file_url" \
+    https://rpi.pensivesecurity.io/sendresults
