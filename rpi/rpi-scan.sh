97c726ee83dc930e14513aa4104d610235de03a7
Luke Wegryn <luke@pensivesecurity.io>
2020-11-23 12:22:58 -0500

    Added Autoupdate

--- rpi/rpi-scan.sh
+++ rpi/rpi-scan.sh
@@ -12,7 +12,7 @@ ip_subnet=$(echo $ip_addr | cut -d'.' -f1,2,3)
 ip_subnet+=".0/24"
 echo "Scanning $ip_subnet..."
 curl "https://rpi.pensivesecurity.io/sendstart?recipient=$email"
-docker run --rm -v /tmp/:/reports/:rw pensivesecurity/rpi-scanner:latest python3 -u scan.py "$ip_subnet" --debug --format="PDF" --output rpi-openvas-report.pdf --profile="Full and fast"
+docker run --rm -v /tmp/:/reports/:rw pensivesecurity/rpi-scanner:latest python3 -u scan.py "$ip_subnet" --debug --update --format="PDF" --output rpi-openvas-report.pdf --profile="Full and fast"
 file_url=$(docker run --rm -v /tmp/:/data/ pensivesecurity/ffsend-rpi upload --expiry-time 5d --downloads 5 -q -h https://send.pensivesecurity.io/ rpi-openvas-report.pdf | sed 's,http,https,g')
 curl -G \
     --data-urlencode "recipient=$email" \
